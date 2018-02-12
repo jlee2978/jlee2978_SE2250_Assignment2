da29b52b01ae970b69ba2414e04af715b5faae99
Jeffrey <jlee2978@uwo.ca>
2018-02-12 02:11:45 -0500

    Completed Step 4

---
+++ jlee2978_SE2250_Assignment2/Assets/Scripts/Cube.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    private int points = 0;
+
+    private Vector3[] vectors = new Vector3[8];
+    private Color[] color = new Color[2];
+
+	// Use this for initialization
+	void Start () {
+        color[0] = Color.yellow;
+        color[1] = Color.gray;
+
+        // positions of pickup object
+        vectors[0] = new Vector3(3, 1.02f, 0);
+        vectors[1] = new Vector3(3, 1.02f, 2);
+        vectors[2] = new Vector3(0, 1.02f, 4);
+        vectors[3] = new Vector3(-3, 1.02f, 2);
+        vectors[4] = new Vector3(-1, 1.02f, 0);
+        vectors[5] = new Vector3(-3, 1.02f, -2);
+        vectors[6] = new Vector3(0, 1.02f, -4);
+        vectors[7] = new Vector3(2, 1.02f, -3);
+    }
+
+    // Update is called once per frame
+    /*void Update () {
+		
+	}*/
+    public void setUp(int i)
+    {
+        switch (i)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                setPoints(100);
+                break;
+            case 0:
+            case 2:
+            case 4:
+            case 6:
+                setPoints(500);
+                break;
+        }
+    }
+
+    public void setPoints(int points)
+    {
+        this.points = points;
+    }
+
+    public int getPoints()
+    {
+        return this.points;
+    }
+}
--- jlee2978_SE2250_Assignment2/Assets/Scripts/Movement.cs
+++ jlee2978_SE2250_Assignment2/Assets/Scripts/Movement.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Movement : MonoBehaviour {
 
+    private GameObject[] cube = null;
+    public GameObject pickup;
+
     public float speed;
 
     private Rigidbody rb;
@@ -14,9 +17,40 @@ public class Movement : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        Vector3[] vectors = new Vector3[8];
+        Color[] color = new Color[2];
+
+        color[0] = Color.yellow;
+        color[1] = Color.gray;
+
+        // positions of pickup object
+        vectors[0] = new Vector3(3, 1.02f, 0);
+        vectors[1] = new Vector3(3, 1.02f, 2);
+        vectors[2] = new Vector3(0, 1.02f, 4);
+        vectors[3] = new Vector3(-3, 1.02f, 2);
+        vectors[4] = new Vector3(-1, 1.02f, 0);
+        vectors[5] = new Vector3(-3, 1.02f, -2);
+        vectors[6] = new Vector3(0, 1.02f, -4);
+        vectors[7] = new Vector3(2, 1.02f, -3);
+
         rb = GetComponent<Rigidbody>();
         score = 0;
         SetScoreText();
+
+        cube = new GameObject[8];
+
+        for (int i = 0; i < 8; i++)
+        {
+            cube[i] = GameObject.Instantiate(pickup);
+            cube[i].transform.position = vectors[i];
+            cube[i].GetComponent<Renderer>().material.color = color[i % 2];
+
+            cube[i].gameObject.SetActive(true);
+
+            var cubeScript = cube[i].GetComponent<Cube>();
+
+            cubeScript.setUp(i);
+        }
     }
 
     void FixedUpdate ()
@@ -29,12 +63,12 @@ public class Movement : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collect)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        if (collect.gameObject.CompareTag("Pickup"))
         {
-            other.gameObject.SetActive(false);
-            score++;        //this will need to be changed later
+            collect.gameObject.SetActive(false);
+            score += collect.gameObject.GetComponent<Cube>().getPoints();        
             SetScoreText();
         }
     }
